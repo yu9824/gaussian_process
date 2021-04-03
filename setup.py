cec4eb81d09750ff6e0c8944801667c1b3e562e8
Yudai IWAMIZU <yu.9824@gmail.com>
2021-04-03 15:14:19 +0900

    Update version(v0.0.2)

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='gaussian_process',
-    version='0.0.1',
+    version='0.0.2',
     description='',
     author='yu-9824',
-    author_email='yu.9824@gmail.com',
+    author_email='yu.9824{0}gmail.com'.format('@'),
     install_requires=install_requirements,
     url='https://github.com/yu-9824/gaussian_process',
     license=license,
